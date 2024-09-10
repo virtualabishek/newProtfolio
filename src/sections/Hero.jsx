cfaf3e4b0d91518e4635afa4d46e447d5619aeda
virtualabishek <717abishekneupane@gmail.com>
2024-09-10 13:45:28 +0545

    initial

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,26 @@
+import { PerspectiveCamera } from "@react-three/drei";
+import React from "react";
+import HackerRoom from "../components/HackerRoom";
+
+const Hero = () => {
+  return (
+    <section className="min-h-screen w-full flex flex-col relative">
+      <div className="w-full mx-auto flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+          Hi, I am Abishek <span className="waving-hand">👋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">
+          Learning Building Projects
+        </p>
+        <div className="w-full h-full absolute inset-0">
+          <Canvas className="w-full h-full">
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <HackerRoom />
+          </Canvas>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
